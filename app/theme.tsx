cd74c891d4887fc5e9431cf3b96d7b193da8b579
bialak <klauiabiaa@gmail.com>
2024-08-19 12:54:13 +0200

    Inicialization project portfolio

---
+++ app/theme.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { createTheme } from "@mui/material/styles";
+import "@/app/globals.css";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1ca8cc",
+      dark: "#0e728c",
+    },
+  },
+});
+
+export default theme;
