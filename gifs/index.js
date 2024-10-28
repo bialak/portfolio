9329b20486bf41968582c930090abc2668ec04c5
bialak <klauiabiaa@gmail.com>
2024-10-28 15:02:50 +0100

    add file to import gifs more legibly

---
+++ gifs/index.js
@@ -0,0 +1,4 @@
+export { default as gamesCrud } from "@/gifs/games-crud.gif";
+export { default as countriesAndCapitals } from "@/gifs/countries-and-capitals.gif";
+export { default as pawsBaner } from "@/gifs/paws-baner.gif";
+export { default as weatherForecast } from "@/gifs/weather-forecast.gif";
